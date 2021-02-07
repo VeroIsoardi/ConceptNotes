5e7569a42c533c71ba29235e20585b97acdc2dde
Vero Isoardi <vai.isoardi@gmail.com>
2021-02-07 12:57:17 -0300

    Export funcionality finished

--- Concept/app/controllers/previews_controller.rb
+++ Concept/app/controllers/previews_controller.rb
@@ -7,15 +7,23 @@ class PreviewsController < ApplicationController
 
   def export
     @html = CommonMarker.render_html("# TODAS LAS NOTAS", :DEFAULT)
+    @html << CommonMarker.render_html('CUADERNOS', :DEFAULT)
     current_user.books.each do |book|
-      @html << CommonMarker.render_html(book.title, :DEFAULT)
+      @html << CommonMarker.render_html("***#{book.title}***", :DEFAULT)
       book.notes.each do |note|
         @html << CommonMarker.render_html('***', :DEFAULT)
         @html << CommonMarker.render_html("**#{note.title}**", :DEFAULT)
         @html << CommonMarker.render_html('---', :DEFAULT)
         @html << CommonMarker.render_html(note.content, :DEFAULT)  
       end
+      @html << CommonMarker.render_html('***', :DEFAULT)
     end
-  end
-  
+    @html << CommonMarker.render_html('NOTAS', :DEFAULT)
+    current_user.notes.where(book_id: nil).each do |note|
+      @html << CommonMarker.render_html('***', :DEFAULT)
+      @html << CommonMarker.render_html("**#{note.title}**", :DEFAULT)
+      @html << CommonMarker.render_html('---', :DEFAULT)
+      @html << CommonMarker.render_html(note.content, :DEFAULT)  
+    end
+  end  
 end
