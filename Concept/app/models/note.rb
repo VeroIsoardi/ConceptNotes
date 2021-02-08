30f9cb5f434f47165a3a2f1990146afaf77d010a
Vero Isoardi <vai.isoardi@gmail.com>
2021-02-08 12:01:14 -0300

    Bug fixed, user can now create note related to a notebook and edit it

--- Concept/app/models/note.rb
+++ Concept/app/models/note.rb
@@ -1,6 +1,6 @@
 class Note < ApplicationRecord
   belongs_to :user, inverse_of: :notes 
   belongs_to :book, inverse_of: :notes, optional: true
-  validates :title, presence: true, length: {maximum: 255}, uniqueness: { scope: :book_id}
+  validates :title, presence: true, length: {maximum: 255}, uniqueness: { scope: [:book_id, :user_id]}
   validates :content, presence: true
 end
